f8c5f5e206e4937e84b1bfbeecfc4b504a1607f6
zedzior <piotr.zabieglik@gmail.com>
2019-12-30 16:57:43 +0100

    adding single city view in search view

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask, session, render_template, request
 from flask_session import Session
-from panda import get_results
+from panda import get_results, get_results_for_city
 
 app = Flask(__name__)
 app.config['SESSION_PERMANENT'] = False
@@ -12,3 +12,9 @@ Session(app)
 def index():
     offers = get_results()
     return render_template("index.html", offers=offers)
+
+
+@app.route("/<string:city>", methods=["POST", "GET"])
+def city(city):
+    offers = get_results_for_city(city)
+    return render_template("city.html", offers=offers)
--- panda.py
+++ panda.py
@@ -4,7 +4,7 @@ import pandas as pd
 def get_results():
     df = pd.read_csv('assets/csv/offers.csv')
     df['price_per_head'] = df.total_price / df.number_people
-    df = df.sort_values('total_price')[['price_per_head', 'ft_to_city_name', 'stay_days']]
+    df = df.sort_values('total_price')[['price_per_head', 'ft_to_city_name', 'stay_days', 'b_link']]
     cities = df.ft_to_city_name.unique()
     result = []
     for city in cities:
@@ -13,5 +13,13 @@ def get_results():
     return result
 
 
+def get_results_for_city(city):
+    df = pd.read_csv('assets/csv/offers.csv')
+    df = df[df.ft_to_city_name == city]
+    df['price_per_head'] = df.total_price / df.number_people
+    df = df.sort_values('total_price')[['price_per_head', 'ft_to_city_name', 'stay_days', 'b_link']]
+    return df.values.tolist()
+
+
 if __name__ == '__main__':
-    print(get_results())
+    print(get_results_for_city('Krakow'))
